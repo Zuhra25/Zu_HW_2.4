f07021acbb0ff45109ec2e8d7d0e8ad305fdf865
ZU <zshaihova@gmail.com>
2023-06-30 17:58:18 +0300

    сделано ДЗ (если 1 метод)

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,32 @@
 
 public class Main {
     public static void main(String[] args) {
-        String login = "java_skypro_go";
+        String login = "java_skypro_goппп";
         String password = "D_1hWiKjjP_9";
         String confirmPassword = "D_1hWiKjjP_9";
 
         checkExceptions(login, password, confirmPassword);
     }
 
-    public static void test(String log, String pass, String confirmPass) {
-        if (log.length() > 20 || !log.matches("\\w+")) {
-            throw new WrongLoginException();
-        }
-        if (pass.length() > 20 || !pass.matches("\\w+") || !pass.matches(confirmPass)) {
-            throw new WrongPasswordException();
-        }
-
-    }
+//    public static void test(String log, String pass, String confirmPass) {
+//        if (log.length() > 20 || !log.matches("\\w+")) {
+//            throw new WrongLoginException();
+//        }
+//        if (pass.length() > 20 || !pass.matches("\\w+") || !pass.matches(confirmPass)) {
+//            throw new WrongPasswordException();
+//        }
+//
+//    }
 
     public static void checkExceptions(String log, String pass, String confirmPass) {
         try {
-            test(log, pass, confirmPass);
+            if (log.length() > 20 || !log.matches("\\w+")) {
+                throw new WrongLoginException();
+            }
+            if (pass.length() > 20 || !pass.matches("\\w+") || !pass.matches(confirmPass)) {
+                throw new WrongPasswordException();
+            }
+//            test(log, pass, confirmPass);
         } catch (WrongLoginException e) {
             System.out.println("не верный формат логина");
             throw new WrongLoginException();
